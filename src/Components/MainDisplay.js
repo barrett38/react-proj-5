fff200fbc6412951b5f0cdf8bddae35f7caa5502
Barrett Anderson <barrett1614@gmail.com>
2024-03-18 13:39:13 -0500

    Fine-tuned 'Weather' button

--- src/Components/MainDisplay.js
+++ src/Components/MainDisplay.js
@@ -2,16 +2,22 @@ import React, { useState } from "react";
 import Overview from "./Overview";
 import Weather from "./Weather";
 import Symbols from "./Symbols";
+import { useSelector } from "react-redux";
+import { selectDisplay } from "../redux/slices/displayCountrySlice";
 
-// display of country selected from search
+// Display of country selected from search
 const MainDisplay = () => {
+  // Getting the capital of the selecetd country
+  let currentDisplay = useSelector(selectDisplay);
+  const givenCapital = currentDisplay.capital;
+  // Making the "Overveiw" the first default view with useState above
   const [view, setView] = useState("Overview");
   return (
     <div className="stack">
       <div className="tab-select">
         <button onClick={() => setView("Overview")}>Overview</button>
         <button onClick={() => setView("Weather")}>
-          Current Weather at Capitol
+          Current Weather in {givenCapital}
         </button>
         <button onClick={() => setView("Symbols")}>Symbols</button>
       </div>
